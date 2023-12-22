8d4854d99cf3092c1aef958dea009b35144b56a6
ShowGa <peter1997217123@gmail.com>
2023-12-23 01:33:52 +0800

    API_URL change

--- src/services/course-service.js
+++ src/services/course-service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/courses";
+const API_URL = "https://coursemernserver.onrender.com/api/courses";
 
 class CourseService {
   get() {
